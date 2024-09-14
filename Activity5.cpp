7cf570e43e8517000a1d615afe53134e1782f559
Zak <zakariabouaddou@gmail.com>
2024-09-13 22:07:24 -0700

    testing for double size and limits

--- Activity5.cpp
+++ Activity5.cpp
@@ -6,5 +6,8 @@ using namespace std;
 
 int main()
 {
-	cout << sizeof(int) << endl;
+	cout << "Max Limit for short data type: " << numeric_limits<short>::max() << endl;
+	cout << "Min Limit for short data type: " << numeric_limits<short>::min() << endl;
+	cout << "Size of short data type: " << sizeof(short) << endl;
+
 }
