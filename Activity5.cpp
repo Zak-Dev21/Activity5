e5a3b19dd69170e6ddb33b3c00c8ab6ffe9f1c12
Zak <zakariabouaddou@gmail.com>
2024-09-13 22:10:48 -0700

    seeing limits and size for double data type aswell

--- Activity5.cpp
+++ Activity5.cpp
@@ -10,4 +10,8 @@ int main()
 	cout << "Min Limit for short data type: " << numeric_limits<short>::min() << endl;
 	cout << "Size of short data type: " << sizeof(short) << endl;
 
+	cout << "Max Limit for double data type: " << numeric_limits<double>::max() << endl;
+	cout << "Min Limit for double data type: " << numeric_limits<double>::min() << endl;
+	cout << "Size of short data type: " << sizeof(double) << endl;
+
 }
